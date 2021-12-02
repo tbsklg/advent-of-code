ea8bd1107e148edfc03347ae23ea3c90509bda9f
tbsklg <tbs.klg@gmail.com>
2021-12-02 20:26:27 +0100

    Solve day 1: sonar sweep

---
+++ src/AdventOfCode/SonarSweep.hs
@@ -0,0 +1,7 @@
+module AdventOfCode.SonarSweep (countIncreasingWindows) where
+
+countIncreasingWindows :: (Ord a, Num p, Num a) => Int -> [a] -> p
+countIncreasingWindows _ [] = 0
+countIncreasingWindows n l
+  | (sum . take n . tail $ l) > (sum . take n $ l) = (+) 1 . countIncreasingWindows n $ tail l
+  | otherwise = countIncreasingWindows n $ tail l
