12ed9ef7a84f93f2bc59a9eaed34297d59717ffc
tbsklg <tbs.klg@gmail.com>
2021-12-12 10:47:59 +0100

    Refactor day nine

--- src/Day9.hs
+++ src/Day9.hs
@@ -25,16 +25,15 @@ basin rows =
 riskLevels :: [[Char]] -> [(Point, Int)]
 riskLevels lines = capture lines 0 0
   where
-    capture lines columnIndex rowIndex
-      | columnIndex == rows = []
-      | rowIndex == columns = capture lines (columnIndex + 1) 0
-    capture lines columnIndex rowIndex
-      | current < minimum neighbours' = (Point rowIndex columnIndex, current) : capture lines columnIndex (rowIndex + 1)
-      | otherwise = capture lines columnIndex (rowIndex + 1)
+    capture lines rowIndex columnIndex
+      | rowIndex == rows = []
+      | columnIndex == columns = capture lines (rowIndex + 1) 0
+    capture lines rowIndex columnIndex
+      | current < minimum currentNeighbours = (Point columnIndex rowIndex, current) : capture lines rowIndex (columnIndex + 1)
+      | otherwise = capture lines rowIndex (columnIndex + 1)
       where
-        neighbours' = map snd . neighbours (Point rowIndex columnIndex) $ lines
-        current = valueOf columnIndex rowIndex
-        valueOf r c = asInt (lines !! r !! c)
+        currentNeighbours = map snd . neighbours (Point columnIndex rowIndex) $ lines
+        current = lookup' (Point columnIndex rowIndex) lines
 
     columns = length . head $ lines
     rows = length lines
@@ -43,29 +42,45 @@ basins :: (Point, Int) -> [[Char]] -> [Point]
 basins p rows = capture [p] empty
   where
     capture [] visited = toList visited
-    capture ((point, value) : xs) l = capture (xs ++ neighbours') (insert point l)
+    capture ((point, value) : xs) visited = capture (xs ++ currentNeighbours) (insert point visited)
       where
-        neighbours' = filter (\(point, _) -> point `notElem` toList l) . filter (\(_, value) -> value /= 9) $ (neighbours point rows)
+        currentNeighbours =
+          filter notYetVisited
+            . filter notNine
+            $ neighbours point rows
+
+        notYetVisited (point, _) = point `notElem` toList visited
+        notNine (_, value) = value /= 9
 
 neighbours :: Point -> [[Char]] -> [(Point, Int)]
-neighbours (Point rowIndex columnIndex) lines = catMaybes [left, right, up, down]
+neighbours (Point columnIndex rowIndex) lines = catMaybes [left, right, up, down]
   where
     left
-      | rowIndex - 1 < 0 = Nothing
-      | otherwise = Just (Point (rowIndex - 1) columnIndex, valueOf columnIndex (rowIndex - 1))
-    right
-      | rowIndex + 1 >= columns = Nothing
-      | otherwise = Just (Point (rowIndex + 1) columnIndex, valueOf columnIndex (rowIndex + 1))
-    up
       | columnIndex - 1 < 0 = Nothing
-      | otherwise = Just (Point rowIndex (columnIndex -1), valueOf (columnIndex - 1) rowIndex)
+      | otherwise = Just (point, lookup' point lines)
+      where
+        point = Point (columnIndex - 1) rowIndex
+    right
+      | columnIndex + 1 >= columns = Nothing
+      | otherwise = Just (point, lookup' point lines)
+      where
+        point = Point (columnIndex + 1) rowIndex
+    up
+      | rowIndex - 1 < 0 = Nothing
+      | otherwise = Just (point, lookup' point lines)
+      where
+        point = Point columnIndex (rowIndex - 1)
     down
-      | columnIndex + 1 >= rows = Nothing
-      | otherwise = Just (Point rowIndex (columnIndex + 1), valueOf (columnIndex + 1) rowIndex)
+      | rowIndex + 1 >= rows = Nothing
+      | otherwise = Just (point, lookup' point lines)
+      where
+        point = Point columnIndex (rowIndex + 1)
 
     columns = length . head $ lines
     rows = length lines
-    valueOf r c = asInt (lines !! r !! c)
+
+lookup' :: Point -> [[Char]] -> Int
+lookup' (Point columnIndex rowIndex) lines = asInt (lines !! rowIndex !! columnIndex)
 
 asInt :: Char -> Int
 asInt x = read [x] :: Int
