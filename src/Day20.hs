492bc2380bd9ff342d1434d7f70f2a1f291426b3
tbsklg <tbs.klg@gmail.com>
2022-01-04 11:41:22 +0100

    Determine Pixels for given coordinates; Calculate ieaIndex based on these pixels

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,7 +1,57 @@
 module Day20 where
+import Data.Char
+
+data Coordinate = Coordinate Int Int deriving (Show, Eq, Ord)
 
 extract :: [[Char]] -> (String, [String])
 extract raw = (iea, img)
-    where
-        iea = head raw
-        img = tail . tail $ raw
+  where
+    iea = head raw
+    img = tail . tail $ raw
+
+determinePixels :: Coordinate -> [[Char]] -> [[Char]]
+determinePixels (Coordinate x y) img
+  | x > rows || y > columns = ["...", "...", "..."]
+  | otherwise = [upper, middle, lower]
+  where
+    current = lookup' (Coordinate x y) img
+    left
+      | y - 1 < 0 = '.'
+      | otherwise = lookup' (Coordinate x (y -1)) img
+    right
+      | y + 1 >= columns = '.'
+      | otherwise = lookup' (Coordinate x (y + 1)) img
+    upLeft
+      | y - 1 < 0 || x - 1 < 0 = '.'
+      | otherwise = lookup' (Coordinate (x -1) (y -1)) img
+    up
+      | x - 1 < 0 = '.'
+      | otherwise = lookup' (Coordinate (x -1) y) img
+    upRight
+      | x - 1 < 0 || y + 1 >= columns = '.'
+      | otherwise = lookup' (Coordinate (x - 1) (y + 1)) img
+    downRight
+      | x + 1 >= rows || y + 1 >= columns = '.'
+      | otherwise = lookup' (Coordinate (x + 1) (y + 1)) img
+    down
+      | x + 1 >= rows = '.'
+      | otherwise = lookup' (Coordinate (x + 1) y) img
+    downLeft
+      | x + 1 >= rows || y - 1 < 0 = '.'
+      | otherwise = lookup' (Coordinate (x + 1) (y - 1)) img
+
+    upper = [upLeft] ++ [up] ++ [upRight]
+    middle = [left] ++ [current] ++ [right]
+    lower = [downLeft] ++ [down] ++ [downRight]
+
+    columns = length . head $ img
+    rows = length img
+
+ieaIndex :: [[Char]] -> Int
+ieaIndex = toDec . map (\x -> if x == '.' then '0' else '1') . concat
+
+toDec :: String -> Int
+toDec = foldl (\acc x -> acc * 2 + digitToInt x) 0
+
+lookup' :: Coordinate -> [String] -> Char
+lookup' (Coordinate x y) img = img !! x !! y
--- test/Day20Spec.hs
+++ test/Day20Spec.hs
@@ -1,10 +1,24 @@
 module Day20Spec where
 
-import Day20 (extract)
 import Test.Hspec (Spec, shouldBe, it)
+import Day20 (Coordinate(..), extract, determinePixels, ieaIndex)
 
 spec :: Spec
 spec = do
   it "should extract iea and image" $ do
     extract ["..#.#..#####.#.#.#.###.##.....###.##.#..###.####..####", "", "#..#.", "#....", "##..#"] 
         `shouldBe` ("..#.#..#####.#.#.#.###.##.....###.##.#..###.####..####", ["#..#.", "#....", "##..#"])
+  
+  it "should return relevant pixels from image for given coordinates" $ do
+    determinePixels (Coordinate 0 0) ["#..#.", "#....", "##..#"] `shouldBe` ["...", ".#.", ".#."]
+    determinePixels (Coordinate 0 1) ["#..#.", "#....", "##..#"] `shouldBe` ["...", "#..", "#.."]
+    determinePixels (Coordinate 1 2) ["#..#.", "#....", "##..#"] `shouldBe` ["..#", "...", "#.."]
+    determinePixels (Coordinate 0 10) ["#..#.", "#....", "##..#"] `shouldBe` ["...", "...", "..."]
+    determinePixels (Coordinate 10 0) ["#..#.", "#....", "##..#"] `shouldBe` ["...", "...", "..."]
+    determinePixels (Coordinate 1 0) ["#..#.", "#....", "##..#"] `shouldBe` [".#.", ".#.", ".##"]
+    determinePixels (Coordinate 2 1) ["#..#.", "#....", "##..#"] `shouldBe` ["#..", "##.", "..."]
+
+  it "should calculate iea index for relevant pixels" $ do
+    ieaIndex ["...", ".#.", ".#."] `shouldBe` 18
+    ieaIndex ["...", "#..", "#.."] `shouldBe` 36
+    ieaIndex ["...", "...", "..."] `shouldBe` 0
