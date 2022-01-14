853f5ef7ec06ac8d33dfc9523554500809062e15
tbsklg <tbs.klg@gmail.com>
2022-01-14 11:36:25 +0100

    Extract raw data

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,27 +1,19 @@
 module Day22 where
 
-import Data.List.Split (splitOn)
 import qualified Data.Set as S
+
+import Data.List.Split (splitOn)
 import Day19 (offset)
-import Debug.Trace
 
 type Cube = (Int, Int, Int)
 
 data Step = Step {isOn :: Bool, x :: (Int, Int), y :: (Int, Int), z :: (Int, Int)} deriving (Show, Ord, Eq)
 
 solve :: [[Char]] -> Int
-solve = countCubes .atLeast50 . extract
+solve s = 5
 
 extract :: [[Char]] -> [Step]
 extract = map step
-
-countCubes :: [Step] -> Int
-countCubes = length . foldl update []
-
-update :: [Cube] -> Step -> [Cube]
-update c s
-  | isOn s = turnOn c . cubes $ s
-  | otherwise = turnOff c . cubes $ s
 
 step :: [Char] -> Step
 step raw = Step {isOn = isOn, x = x, y = y, z = z}
@@ -31,24 +23,12 @@ step raw = Step {isOn = isOn, x = x, y = y, z = z}
     z = ranges . last . splitOn "," . last . words $ raw
     isOn = (==) "on" . head . words $ raw
 
-numberOfCubes :: Step -> Int
-numberOfCubes Step {x = (fx, tx), y = (fy, ty), z = (fz, tz) } = (tx - fx + 1) * (ty - fy + 1) * (tz - fz + 1)
-
 ranges :: [Char] -> (Int, Int)
 ranges raw = (from, to)
   where
     from = read (head . splitOn ".." $ s) :: Int
     to = read (last . splitOn ".." $ s) :: Int
     s = last . splitOn "=" $ raw
-
-cubes :: Step -> [Cube]
-cubes Step {x = (fx, tx), y = (fy, ty), z = (fz, tz)} = [(x', y', z') | x' <- [fx .. tx], y' <- [fy .. ty], z' <- [fz .. tz]]
-
-turnOn :: [Cube] -> [Cube] -> [Cube]
-turnOn c1 c2 = S.toList . S.fromList $ (c1 ++ c2)
-
-turnOff :: [Cube] -> [Cube] -> [Cube]
-turnOff c1 c2 = filter (`notElem` c2) c1
 
 atLeast50 :: [Step] -> [Step]
 atLeast50 =
